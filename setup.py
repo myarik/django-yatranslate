309eddfc451302702e8459eed9975e609f9cc627
Yaroslav Muravskiy <myarik@gmail.com>
2015-12-23 19:36:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='django-yatranslate',
     version='0.1',
     description='Simple translate',
-    author='Yaroslav Muravsky',
+    author='Yaroslav Muravskiy',
     author_email='y@myarik.com',
     url='http://github.com/myarik/django-yatranslate.git',
     py_modules=['django', 'grab', 'pycurl', 'lxml'],
